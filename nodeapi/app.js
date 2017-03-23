edb172709d51120c27939a2ac69da0aef55c23c5
Javi Miguel <jamg44@gmail.com>
2017-03-23 22:37:49 +0100

    empezando con express

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -22,8 +19,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// ejemplo de middleware de log
+app.use(function(req, res, next) {
+  console.log(req.host);
+  next();
+});
+
+app.use('/',      require('./routes/index'));
+app.use('/users', require('./routes/users'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
